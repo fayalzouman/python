cb68968e540f665fc8cf236577e7491ba3b448be
Fay <fayalzouman@gmail.com>
2019-06-27 15:37:56 +0300

    run

--- string_task.py
+++ string_task.py
@@ -1,7 +1,7 @@
 time = raw_input("What is your favorite number?")
-items = raw_input("What is the item on your right?")
-name = raw_input("What's your name?")
+item = raw_input("What is the item on your right?")
+name = raw_input("What's your friend's name?")
 scream = raw_input("What do you yell when you want to scream?")
 action = raw_input("What would you do if you got scared?")
 
-print "It is %s in the morning and %s was sleeping. You pick up a %s and yell "%s!" %s was so scared, that they started to %s" %(time, name.upper()[0], item, scream.upper(), name, action)
+print "It is %s in the morning and %s was sleeping. You pick up a %s and yell ""%s!"" %s was so scared, that they started to %s" %(time, name.capitalize(), item, scream.upper(), name, action)
