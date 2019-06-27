875baacd0e43230ea509230c3b59ea5d48b10fde
Fay <fayalzouman@gmail.com>
2019-06-27 16:14:57 +0300

    conditions

--- conditions_task.py
+++ conditions_task.py
@@ -1,22 +1,23 @@
 from math import *
-first_num = raw_input("Write a number from 1-10")
+first_num = raw_input("Write a numbere:")
+second_num = raw_input("Choose a second number:")
 if first_num == float(first_num) or int(second_num):
 	print first_num
 else:
 	print "Please submit a number"
-second_num = raw_input("Write a number from 11-20")
+
 if second_num == float(second_num) or int(second_num):
 	print first_num
 else:
 	print "Please submit a number"
-operation = raw_input("Choose an operation: add, subtract, multiply or divide.")
-if operation == +:
+operation = raw_input("Please type your operation: add, subtract, multiply or divide.")
+if operation = raw_input("add"):
 	print first_num + second_num
-elif operation == -:
+elif operation = raw_input("subtract"):
 	print first_num - second_num
-elif operation == *:
+elif operation = raw_input("multiply"):
 	print first_num * second_num
-elif operation == /:
+elif operation = raw_input("divide"):
 	print first_num / second_num
 else:
 	print "Please choose one of the options."
