f43df92a38ec95d4103030a2c0ab0286234f1e82
Fay <fayalzouman@gmail.com>
2019-06-27 12:26:49 +0300

    string task

---
+++ functions_task.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+now = datetime.now()
+from math import *
+def check_birthdate(year, month, day):
+year = raw_input("What year were you born?")
+month = raw_input("What month were you born?")
+day = raw_input("What day were you born?")
+if check_birthdate >= datetime:
+	print "False"
+else:
+	print "True"
+def calculate_age(year, month, day):
+age = now.year - year
+print age 
+
