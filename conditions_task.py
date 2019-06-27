8d1eb4071e0f63a9e3ce69b47ddf61c6b9ade7db
Fay <fayalzouman@gmail.com>
2019-06-27 11:15:00 +0300

    Wait for operations

---
+++ conditions_task.py
@@ -0,0 +1,12 @@
+first_num = raw_input("Write a number from 1-10")
+if first_num == float(first_num) or int(second_num):
+	print first_num
+else:
+	print "Please submit a number"
+second_num = raw_input("Write a number from 11-20")
+if second_num == float(second_num) or int(second_num):
+	print first_num
+else:
+	print "Please submit a number"
+operation = raw_input("Choose an operation.")
+if operation ==
