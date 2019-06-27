f936dbb298dac6c5640babd3070a3690297638c1
Fay <fayalzouman@gmail.com>
2019-06-27 11:28:54 +0300

    1

--- conditions_task.py
+++ conditions_task.py
@@ -1,3 +1,4 @@
+from math import *
 first_num = raw_input("Write a number from 1-10")
 if first_num == float(first_num) or int(second_num):
 	print first_num
@@ -9,4 +10,4 @@ if second_num == float(second_num) or int(second_num):
 else:
 	print "Please submit a number"
 operation = raw_input("Choose an operation.")
-if operation ==
+if operation 
