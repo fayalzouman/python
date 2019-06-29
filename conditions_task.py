7217d6eabb12c6f03db649f7e5af85c3de11a6b0
Fay <fayalzouman@gmail.com>
2019-06-29 18:27:35 +0300

    Updated

--- conditions_task.py
+++ conditions_task.py
@@ -1,5 +1,5 @@
 from math import *
-first_num = raw_input("Write a numbere:")
+first_num = raw_input("Write a number:")
 second_num = raw_input("Choose a second number:")
 if first_num == float(first_num) or int(second_num):
 	print first_num
@@ -11,13 +11,13 @@ if second_num == float(second_num) or int(second_num):
 else:
 	print "Please submit a number"
 operation = raw_input("Please type your operation: add, subtract, multiply or divide.")
-if operation = raw_input("add"):
+if raw_input("add"):
 	print first_num + second_num
-elif operation = raw_input("subtract"):
+elif raw_input("subtract"):
 	print first_num - second_num
-elif operation = raw_input("multiply"):
+elif raw_input("multiply"):
 	print first_num * second_num
-elif operation = raw_input("divide"):
+elif raw_input("divide"):
 	print first_num / second_num
 else:
 	print "Please choose one of the options."
---
+++ lists_dicts_task.py
@@ -0,0 +1,6 @@
+skills = ["python", "java", "django"]
+cv = {
+	"name" : 
+}
+name = raw_input("What is your name?")
+dictionary["name"] = raw_input("name")
