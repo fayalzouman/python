964ae10e57ea89df5dc1c7390e82519ce202af73
Fay <fayalzouman@gmail.com>
2019-07-01 20:27:49 +0300

    final

--- lists_dicts_task.py
+++ lists_dicts_task.py
@@ -1,16 +1,16 @@
 skills = ["python", "java", "django"]
 cv = {}
-cv["name"] = raw_input("What is your name?")
-cv["age"] = int(raw_input("What is your age?"))
-cv["experience"] = raw_input("How many years of experience do you have?")
+cv["name"] = input("What is your name?")
+cv["age"] = int(input("What is your age?"))
+cv["experience"] = input("How many years of experience do you have?")
 cv["skills"] = {}
 
 print("1." + skills[0] + " 2." + skills[1] + " 3." + skills[2])
 
-askill = raw_input("Pick a skill from above")
+askill = input("Pick a skill from above")
 cv["askill"] = askill
 
-aaskill = raw_input("Pick another skill from above")
+aaskill = input("Pick another skill from above")
 cv["aaskill"] = aaskill
 
 if cv["age"] <= 25:
